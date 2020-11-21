04714b55e00a3e9568394e016433f3828d8a9da5
kato <katonaokichi2@gmail.com>
2020-11-21 12:02:27 +0900

    Complete advanced testing setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get "static_pages/about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing
   # コントローラー名_アクション名_url  という urlヘルパーがつかえる
   # root_url (特殊)
 end
